33e87cd3834f8a3aab37792eb4f961a4549bccc1
afiffahreza <afiffahreza3@gmail.com>
2021-10-18 19:51:29 +0700

    Add menu endpoints to API

--- api.py
+++ api.py
@@ -19,8 +19,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 # Read Json Files
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
-with open("users.json", "r") as read_file:
-    users = json.load(read_file)
+with open("users.json", "r") as read_file2:
+    users = json.load(read_file2)
 
 
 class Token(BaseModel):  # Token class
@@ -41,6 +41,13 @@ class User(BaseModel):  # User Auth
 
 class UserInDB(User):  # User Auth
     hashed_password: str
+
+
+class UserCreate(BaseModel):  # User Auth
+    username: str
+    password: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
 
 
 class ItemCreate(BaseModel):  # Create data transfer class (ID auto increment)
@@ -136,6 +143,100 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+# @app.post("/register", response_model=User)
+# async def register(user: UserCreate):
+#     newUser = user.dict()
+#     userJson = {newUser['username']: {
+#         'username': newUser['username'],
+#         'full_name': newUser['full_name'],
+#         'email': newUser['email'],
+#         'hashed_password': get_password_hash(newUser['password']),
+#         'disabled': False
+#     }}
+#     users['user'].add(dict(userJson))
+#     if userJson:
+#         with open("users.json", "w") as write_file:
+#             json.dump(users, write_file)
+#         write_file.close()
+#         return userJson
+#     raise HTTPException(status_code=400, detail=f'Bad request')
+
+
 @app.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
+
+@app.get('/menu')  # READ ALL MENU
+async def read_all_menu(current_user: User = Depends(get_current_active_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    if data['menu']:
+        return data['menu']
+    raise HTTPException(status_code=404, detail=f'Item not found')
+
+
+@app.get('/menu/{item_id}')  # READ MENY BY ID
+async def read_menu(item_id: int, current_user: User = Depends(get_current_active_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    for menu_item in data['menu']:
+        if menu_item['id'] == item_id:
+            return menu_item
+    raise HTTPException(status_code=404, detail=f'Item not found')
+
+
+@app.post('/menu')  # CREATE MENU
+async def create_menu(item: ItemCreate, current_user: User = Depends(get_current_active_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    id = 1
+    if len(data['menu']) > 0:
+        id += data['menu'][len(data['menu'])-1]['id']
+    item_dict = item.dict()
+    res = {'id': id, 'name': item_dict['name']}
+    data['menu'].append(dict(res))
+    if res:
+        with open("menu.json", "w") as write_file:
+            json.dump(data, write_file)
+        write_file.close()
+        return res
+    raise HTTPException(status_code=400, detail=f'Bad request')
+
+
+@app.delete('/menu/{item_id}')  # DELETE MENU BY ID
+async def delete_meu(item_id: int, current_user: User = Depends(get_current_active_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    item_found = False
+    for data_idx, data_menu in enumerate(data['menu']):
+        if data_menu['id'] == item_id:
+            tmp = data_menu
+            item_found = True
+            data['menu'].pop(data_idx)
+            with open("menu.json", "w") as write_file:
+                json.dump(data, write_file)
+            write_file.close()
+            return tmp
+    if not item_found:
+        return "Menu ID not found"
+    raise HTTPException(status_code=400, detail=f'Item not found')
+
+
+@app.put('/menu/{item_id}')  # UPDATE MENU BY ID
+async def update_menu(item_id: int, item: ItemUpdate, current_user: User = Depends(get_current_active_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    item_dict = item.dict()
+    item_found = False
+    for data_idx, item_data in enumerate(data['menu']):
+        if item_data['id'] == item_id:
+            item_found = True
+            data['menu'][data_idx] = item_dict
+            with open("menu.json", "w") as write_file:
+                json.dump(data, write_file)
+            write_file.close()
+            return item_data
+    if not item_found:
+        return "Menu ID not found"
+    raise HTTPException(status_code=400, detail=f'Item not found')
